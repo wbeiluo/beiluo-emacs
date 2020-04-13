3ef238fdd3ba5d03a7271beca66902a0b4a05996
wbeiluo <wbeiluo@gmail.com>
2020-04-13 22:27:05 +0800

    增加Windows系统下的字体设置

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -14,10 +14,21 @@
    ((featurep 'cocoa)
     (setq emacs-font-name "Monaco"))
    ((string-equal system-type "gnu/linux")
-    (setq emacs-font-name "WenQuanYi Micro Hei Mono")))
+    (setq emacs-font-name "WenQuanYi Micro Hei Mono"))
+   ((string-equal system-type "windows-nt")
+    (setq emacs-font-name "Consolas")))
   (when (display-grayscale-p)
     (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
     (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))))
+
+(when sys/win32p
+    ;;Chinese Font
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                        charset (font-spec :family "Microsoft YaHei UI"
+                                        :size 18))))
+
+
 
 (provide 'init-fonts)
 
