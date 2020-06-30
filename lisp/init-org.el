2a6b9d7ff88354b38d24063c53a0ea0da326c5a6
wbeiluo <beiluo.wang@139.com>
2020-06-30 15:05:51 +0800

    修改org-mode字体为文泉驿等宽正黑
    完美匹配Linux和Windows系统。Sarasa字体在Windows下很卡。

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -18,7 +18,7 @@
   (defun org-buffer-face-mode-variable ()
     (interactive)
     (make-face 'width-font-face)
-    (set-face-attribute 'width-font-face nil :font "Sarasa Fixed SC 12")
+    (set-face-attribute 'width-font-face nil :font "WenQuanYi Zen Hei Mono 12")
     (setq buffer-face-mode-face 'width-font-face)
     (buffer-face-mode))
   (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
