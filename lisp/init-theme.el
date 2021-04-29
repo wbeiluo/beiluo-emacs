f0dbf88338e4b59ea043b66d971cecb1cd8dbbfd
wbeiluo <beiluo.wang@139.com>
2021-04-30 00:03:01 +0800

    增加亮、暗色主题切换函数

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -8,40 +8,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Install additinal themes from melpa
-;; make sure to use :defer keyword
-;; (use-package doom-themes
-;;   :ensure
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-;;   ;; (if (display-graphic-p)
-;;   ;;     (load-theme 'doom-solarized-dark t)
-;;   ;;   (load-theme 'doom-solarized-light t))
-
-;;   ;; Enable flashing mode-line on errors
-;;   (doom-themes-visual-bell-config)
-
-;;   ;; Enable custom neotree theme (all-the-icons must be installed!)
-;;   (doom-themes-neotree-config)
-;;   ;; or for treemacs users
-;;   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-;;   (doom-themes-treemacs-config)
-
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
-
-;; Switching themes on time of day
-;; (use-package circadian
-;;   :ensure t
-;;   :config
-;;   (setq circadian-themes '(("8:00" . solarized-light)
-;;                            ("19:30" . solarized-dark)))
-;;   (circadian-setup))
-
-
 (use-package solarized-theme
   :ensure t
   :init
@@ -74,38 +40,73 @@
   (setq solarized-height-plus-2 1.0)
   (setq solarized-height-plus-3 1.0)
   (setq solarized-height-plus-4 1.0)
+  )
 
-  :config
-  ;; 加载配置
-  (if (display-graphic-p)
-      (load-theme 'solarized-dark t)
-    (load-theme 'tango t))
+;; dsiable-theme before load-theme
+(defcustom load-theme-before-hook nil
+  "Functions to run before load theme."
+  :type 'hook)
 
-  ;; (set-face-attribute 'mode-line          nil :overline   nil)
-  ;; (set-face-attribute 'mode-line-inactive nil :overline   nil)
-  ;; (set-face-attribute 'mode-line          nil :underline  nil)
-  ;; (set-face-attribute 'mode-line-inactive nil :underline  nil)
-  ;; (set-face-attribute 'mode-line          nil :box        nil)
-  ;; (set-face-attribute 'mode-line-inactive nil :box        nil)
+(defcustom load-theme-after-hook nil
+  "Functions to run after load theme."
+  :type 'hook)
 
-  ;; 取消modeline边框及下划线
+(defun load-theme-hook-wrapper (origin-func theme &rest args)
+  "A wrapper of hooks around `load-theme'."
+  (mapc #'disable-theme custom-enabled-themes)
+  (run-hook-with-args 'load-theme-before-hook theme)
+  (apply origin-func theme args)
+  (run-hook-with-args 'load-theme-after-hook theme))
+
+(advice-add 'load-theme :around #'load-theme-hook-wrapper)
+
+;; 取消modeline边框及下划线, 取消header-line下划线
+(defun del-line-of-modeline()
   (set-face-attribute 'mode-line nil
                       ;; :background "#073642"
                       ;; :foreground "#839496"
                       :box nil
                       :overline nil
                       :underline nil)
+
   (set-face-attribute 'mode-line-inactive nil
                       :box nil
                       :overline nil
                       :underline nil)
 
-  ;; 取消header-line下划线
   (set-face-attribute 'header-line nil
                       :box nil
                       :overline nil
-                      :underline nil)
-  )
+                      :underline nil))
+
+;; switch to light theme
+(defun light ()
+  "Activate a light color theme."
+  (interactive)
+  (load-theme 'solarized-light t)
+  (del-line-of-modeline))
+
+;; switch to dark theme
+(defun dark ()
+  "Activate a dark color theme."
+  (interactive)
+  (load-theme 'solarized-dark t)
+  (del-line-of-modeline))
+
+;; Switching themes on time of day
+;; (if (display-graphic-p)
+;;     (use-package circadian
+;;       :ensure t
+;;       :config
+;;       (setq circadian-themes '(("8:00" . solarized-light)
+;;                                ("19:30" . solarized-dark)))
+;;       (circadian-setup))
+;;   (load-theme 'tango t))
+
+;; default theme
+(if (display-graphic-p)
+    (light)
+  (load-theme 'tsdh-light t))
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
