7b6442a1108b8d8f39b2e9569489fbacdf633c77
wbeiluo <wbeiluo@gmail.com>
2026-01-18 20:55:21 +0800

    简化词典数量，减小占用空间

--- config/init-sdcv.el
+++ config/init-sdcv.el
@@ -1,6 +1,6 @@
 ;;; init-sdcv.el --- Sdcv Configuration. -*- lexical-binding: t -*-
 
-;; Copyright (C) 2025~2025 王北洛
+;; Copyright (C) 2025~2026 王北洛
 
 ;; Author: 王北洛 <wbeiluo@gmail.com>
 ;; URL: https://github.com/wbeiluo/beiluo-emacs
@@ -41,34 +41,16 @@
   ;; 查询失败显示方式
   (setq sdcv-fail-notify-string "未找到词解")
 
-  ;; 简单查询列表
-  (setq sdcv-dictionary-simple-list
-        '("牛津英汉双解美化版"
-          "懒虫简明英汉词典"
-          "朗道汉英字典5.0"
-          "WordNet"
-          "Jargon"))
-
   ;; 完整查询列表
   (setq sdcv-dictionary-complete-list
-        '(
-          "懒虫简明英汉词典"
+        '("懒虫简明英汉词典"
+          "懒虫简明汉英词典"
           "英汉汉英专业词典"
           "XDICT英汉辞典"
-          "stardict1.3英汉辞典"
-          "WordNet"
           "XDICT汉英辞典"
-          "Jargon"
-          "懒虫简明汉英词典"
-          "FOLDOC"
-          "新世纪英汉科技大词典"
-          "KDic11万英汉词典"
-          "朗道汉英字典5.0"
-          "CDICT5英汉辞典"
-          "新世纪汉英科技大词典"
+          "stardict1.3英汉辞典"
           "牛津英汉双解美化版"
-          "21世纪双语科技词典"
-          "quick_eng-zh_CN"
+          "CDICT5英汉辞典"
           )))
 
 (provide 'init-sdcv)
